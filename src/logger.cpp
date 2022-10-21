159e777815e140164afcb0d244248cc482139f9b
Juho Kim <juho-kim@outlook.com>
2022-10-20 22:43:36 -0400

    Implement logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,4 +1,78 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+
 #include "logger.h"
 
-namespace revolution {
+namespace Revolution {
+	const Log_level Log_level::trace("trace", 0);
+	const Log_level Log_level::debug("debug", 1);
+	const Log_level Log_level::info("info", 2);
+	const Log_level Log_level::warning("warning", 3);
+	const Log_level Log_level::error("error", 4);
+	const Log_level Log_level::fatal("fatal", 5);
+
+	const std::string &Log_level::get_name() const
+	{
+		return name;
+	}
+
+	const unsigned int &Log_level::get_severity() const
+	{
+		return severity;
+	}
+
+	Log_level::Log_level(const std::string &name, const unsigned int &severity)
+		: name{name}, severity{severity}
+	{
+	}
+
+	Logger::Logger(const Log_level &log_level, const std::string &filename)
+		: log_level{log_level}, fstream{}
+	{
+		if (!filename.empty()) {
+			fstream.open(filename, std::fstream::out);
+
+			if (fstream.fail()) {
+				log(Log_level::error, "Failed to open log file. Using stdout instead.");
+			}
+		}
+	}
+
+	Logger::~Logger()
+	{
+		if (fstream.is_open()) {
+			fstream.close();
+
+			if (fstream.fail()) {
+				log(Log_level::error, "Failed to close log file.");
+			}
+		}
+	}
+
+	const Log_level &Logger::get_log_level() const
+	{
+		return log_level;
+	}
+
+	void Logger::log(const Log_level& log_level, const std::string &string)
+	{
+		if (log_level.get_severity() < get_log_level().get_severity())
+			return;
+
+		auto time_point = std::chrono::system_clock::now();
+		auto time = std::chrono::system_clock::to_time_t(time_point);
+
+		get_ostream() << '[' << std::put_time(std::localtime(&time), "%c")
+			<< "] [" << log_level.get_name()
+			<< "]: " << string << std::endl;
+	}
+
+	std::ostream &Logger::get_ostream()
+	{
+		if (fstream.is_open())
+			return fstream;
+		else
+			return std::cout;
+	}
 }
--- src/logger.h
+++ src/logger.h
@@ -1,19 +1,42 @@
 #ifndef REVOLUTION_LOGGER_H
 #define REVOLUTION_LOGGER_H
 
-#include <iostream>
+#include <fstream>
+#include <string>
 
-namespace revolution {
-enum class LogLevel {
-  TRACE,
-  DEBUG,
-  INFO,
-  WARNING,
-  ERROR,
-  FATAL
-};
+namespace Revolution {
+	class Log_level {
+	public:
+		static const Log_level trace;
+		static const Log_level debug;
+		static const Log_level info;
+		static const Log_level warning;
+		static const Log_level error;
+		static const Log_level fatal;
 
-typedef std::ostream Logger;
+		const std::string &get_name() const;
+		const unsigned int &get_severity() const;
+	private:
+		explicit Log_level(const std::string &name, const unsigned int &severity);
+
+		const std::string name;
+		const unsigned int severity;
+	};
+
+	class Logger {
+	public:
+		Logger(const Log_level &log_level, const std::string &filename = "");
+		~Logger();
+
+		const Log_level &get_log_level() const;
+
+		void log(const Log_level& log_level, const std::string &string);
+	private:
+		std::ostream &get_ostream();
+
+		const Log_level log_level;
+		std::fstream fstream;
+	};
 }
 
 #endif  // REVOLUTION_LOGGER_H
