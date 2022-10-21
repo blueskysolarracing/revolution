1d25e682426ee41dc2e7d3d26c966026f49ac585
Juho Kim <juho-kim@outlook.com>
2022-10-21 19:09:51 +0000

    complete topology

--- src/topology.cpp
+++ src/topology.cpp
@@ -1,28 +1,141 @@
 #include "topology.h"
 
-namespace revolution {
-Instance Topology::master {
-	"syncer"
-};
-
-std::vector<Instance> Topology::slaves {
+namespace Revolution {
+	Instance::Instance(
+		const std::string &name,
+		const unsigned int &priority,
+		const Log_level &log_level,
+		const std::string &log_filename,
+		const std::string &command
+	) : name{name},
+	    priority{priority},
+	    log_level{log_level},
+	    log_filename{log_filename},
+	    command{command}
 	{
-		"display_driver"
-	},
-	{
-		"miscellanious_controller"
-	},
-	{
-		"motor_controller"
-	},
-	{
-		"power_sensor"
-	},
-	{
-		"telemeter"
-	},
-	{
-		"voltage_controller"
 	}
-};
+
+	const std::string &Instance::get_name() const
+	{
+		return name;
+	}
+
+	const unsigned int &Instance::get_priority() const
+	{
+		return priority;
+	}
+
+	const Log_level &Instance::get_log_level() const
+	{
+		return log_level;
+	}
+
+	const std::string &Instance::get_log_filename() const
+	{
+		return log_filename;
+	}
+
+	const std::string &Instance::get_command() const
+	{
+		return command;
+	}
+
+	Topology::Topology()
+		: syncer{
+			"syncer",
+			0,
+			Log_level::info,
+			"syncer.log",
+			"./syncer"
+		}, display_driver{
+			"display_driver",
+			0,
+			Log_level::info,
+			"display_driver.log",
+			"./display_driver"
+		}, miscellanious_controller{
+			"miscellanious_controller",
+			0,
+			Log_level::info,
+			"miscellanious_controller.log",
+			"./miscellanious_controller"
+		}, motor_controller{
+			"motor_controller",
+			0,
+			Log_level::info,
+			"motor_controller.log",
+			"./motor_controller"
+		}, power_sensor{
+			"power_sensor",
+			0,
+			Log_level::info,
+			"power_sensor.log",
+			"./power_sensor"
+		}, telemeter{
+			"telemeter",
+			0,
+			Log_level::info,
+			"telemeter.log",
+			"./telemeter"
+		}, voltage_controller{
+			"voltage_controller",
+			0,
+			Log_level::info,
+			"voltage_controller.log",
+			"./voltage_controller"
+		}
+	{
+	}
+
+	const Instance &Topology::get_syncer() const
+	{
+		return syncer;
+	}
+
+	const Instance &Topology::get_display_driver() const
+	{
+		return display_driver;
+	}
+
+	const Instance &Topology::get_miscellanious_controller() const
+	{
+		return miscellanious_controller;
+	}
+
+	const Instance &Topology::get_motor_controller() const
+	{
+		return motor_controller;
+	}
+
+	const Instance &Topology::get_power_sensor() const
+	{
+		return power_sensor;
+	}
+
+	const Instance &Topology::get_telemeter() const
+	{
+		return telemeter;
+	}
+
+	const Instance &Topology::get_voltage_controller() const
+	{
+		return voltage_controller;
+	}
+
+	const Instance &Topology::get_master() const
+	{
+		return get_syncer();
+	}
+
+	std::vector<Instance> Topology::get_slaves() const
+	{
+		return {
+			get_display_driver(),
+			get_miscellanious_controller(),
+			get_motor_controller(),
+			get_power_sensor(),
+			get_telemeter(),
+			get_voltage_controller()
+		};
+	}
 }
--- src/topology.h
+++ src/topology.h
@@ -1,33 +1,57 @@
 #ifndef REVOLUTION_TOPOLOGY_H
 #define REVOLUTION_TOPOLOGY_H
 
-#include <iostream>
 #include <vector>
-#include <string>
 
-namespace revolution {
-struct Instance {
-	const std::string name;
+#include "logger.h"
+
+namespace Revolution {
+	class Instance {
+	public:
+		Instance(
+			const std::string &name,
+			const unsigned int &priority,
+			const Log_level &log_level,
+			const std::string &log_filename,
+			const std::string &command
+		);
+
+		const std::string &get_name() const;
+		const unsigned int &get_priority() const;
+		const Log_level &get_log_level() const;
+		const std::string &get_log_filename() const;
+		const std::string &get_command() const;
+	private:
+		const std::string name;
+		const unsigned int priority;
+		const Log_level log_level;
+		const std::string log_filename;
+		const std::string command;
+	};
+
+	class Topology {
+	public:
+		Topology();
+
+		const Instance &get_syncer() const;
+		const Instance &get_display_driver() const;
+		const Instance &get_miscellanious_controller() const;
+		const Instance &get_motor_controller() const;
+		const Instance &get_power_sensor() const;
+		const Instance &get_telemeter() const;
+		const Instance &get_voltage_controller() const;
+
+		const Instance &get_master() const;
+		std::vector<Instance> get_slaves() const;
+	private:
+		const Instance syncer;
+		const Instance display_driver;
+		const Instance miscellanious_controller;
+		const Instance motor_controller;
+		const Instance power_sensor;
+		const Instance telemeter;
+		const Instance voltage_controller;
+	};
 };
-struct Topology {
-
-	static Instance master;
-	static std::vector<Instance> slaves;
-	static Instance null;
-
-	static const Instance &getInstance(const std::string &name) {
-		if (master.name == name)
-			return master;
-
-		for (const auto &slave : slaves)
-			if (slave.name == name)
-				return slave;
-
-		std::cout << "ERROR instance not found" << std::endl;
-
-		return null;
-	}
-};
-}
 
 #endif  // REVOLUTION_TOPOLOGY_H
