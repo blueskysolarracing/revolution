8e9bf6dd07354e6a89e4f04f046a34197650c27c
Juho Kim <juho-kim@outlook.com>
2022-10-22 11:53:20 -0400

    rework client

--- src/client.cpp
+++ src/client.cpp
@@ -1,64 +1,102 @@
-#include <thread>
+#include <iostream>
 #include <sstream>
+#include <thread>
 
 #include "client.h"
 
 namespace Revolution {
-Client::Client(const std::string &name, const std::string &recipientName)
-	: Application{name}, recipientName_{recipientName} {
+	Client::Client(const std::string &sender_name, const std::string &recipient_name)
+		: Application{Instance{sender_name, get_priority(), get_log_level(), get_log_filename(), get_command(sender_name, recipient_name)}},
+		  recipient_name{recipient_name}
+	{
+	}
+
+	void Client::run()
+	{
+		std::thread thread(&Client::help_run, this);
+		std::string input;
+
+		while (std::getline(std::cin, input)) {
+			std::istringstream iss(input);
+			std::string header_name;
+			std::string datum;
+			std::vector<std::string> data;
+
+			iss >> header_name;
+
+			while (std::getline(iss, datum, ' '))
+				if (!datum.empty())
+					data.push_back(datum);
+
+			if (!header_name.empty())
+				get_messenger().send(get_recipient_name(), header_name, data);
+		}
+	}
+
+	const unsigned int Client::priority{0};
+	const Log_level Client::log_level{Log_level::info};
+	const std::string Client::log_filename{""};
+
+	const unsigned int &Client::get_priority()
+	{
+		return priority;
+	}
+
+	const Log_level &Client::get_log_level()
+	{
+		return log_level;
+	}
+
+	const std::string &Client::get_log_filename()
+	{
+		return log_filename;
+	}
+
+	const std::string Client::get_command(std::string sender_name, std::string recipient_name)
+	{
+		return "./client.cpp " + sender_name + " " + recipient_name;
+	}
+
+	const std::string &Client::get_recipient_name() const
+	{
+		return recipient_name;
+	}
+
+	void Client::help_run()
+	{
+		for (;;) {
+			auto message = get_messenger().receive();
+
+			std::cout << message.get_sender_name() << ": "
+				<< message.get_header_name() << '(';
+
+			auto &data = message.get_data();
+
+			if (!data.empty()) {
+				std::cout << data[0];
+
+				for (auto it = data.begin() + 1; it != data.end(); ++it)
+					std::cout << ", " << *it;
+			}
+
+			std::cout << ')' << std::endl;
+		}
+	}
 }
 
-const std::string &Client::getRecipientName() const {
-	return recipientName_;
-}
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		std::cout << "Invalid number of arguments" << std::endl;
+		std::cout << "Usage: sender_name recipient_name" << std::endl;
+		return 0;
+	}
 
-void Client::run() {
-  std::thread thread(&Client::helpRun, this);
-  std::string line;
+	std::string senderName(argv[1]);
+	std::string recipientName(argv[2]);
+	Revolution::Client client{senderName, recipientName};
 
-  while (std::getline(std::cin, line)) {
-    if (line.empty())
-	    continue;
+	client.run();
 
-    std::istringstream iss(line);
-    std::string typeName;
-    std::string argument;
-    std::vector<std::string> arguments;
-
-    iss >> typeName;
-
-    while (std::getline(iss, argument, ' '))
-      if (!argument.empty())
-        arguments.push_back(argument);
-
-    getMessageQueue().send(getRecipientName(), typeName, arguments);
-  }
-}
-
-void Client::helpRun() {
-  for(;;) {
-    auto message = getMessageQueue().receive();
-    auto &logger = getLogger(LogLevel::INFO);
-
-    logger << message.getSenderName()
-	    << ": " << message.getTypeName() << '(';
-
-    auto arguments = message.getArguments();
-    
-    for (auto &argument : arguments)
-	    logger << argument << ", ";
-
-    logger << ')' << std::endl;
-  }
-}
-}
-
-int main(int argc, char *argv[]) {
-  if (argc != 3) {std::cout << "invalid arg count" << std::endl; return 0; }
-  std::string name(argv[1]), recipientName(argv[2]);
-  revolution::Client client{name, recipientName};
-
-  client.run();
-
-  return 0;
+	return 0;
 }
--- src/client.h
+++ src/client.h
@@ -4,18 +4,30 @@
 #include "application.h"
 
 namespace Revolution {
-class Client : public Application {
-public:
-  explicit Client(const std::string &name, const std::string &recipientName);
+	class Client : public Application {
+	public:
+		explicit Client(
+			const std::string &sender_name,
+			const std::string &recipient_name
+		);
 
-  const std::string &getRecipientName() const;
+		void run() override;
+	private:
+		static const unsigned int priority;
+		static const Log_level log_level;
+		static const std::string log_filename;
 
-  void run() override;
-private:
-  void helpRun();
+		static const unsigned int &get_priority();
+		static const Log_level &get_log_level();
+		static const std::string &get_log_filename();
+		static const std::string get_command(std::string sender_name, std::string recipient_name);
 
-  std::string recipientName_;
-};
+		const std::string &get_recipient_name() const;
+
+		void help_run();
+
+		std::string recipient_name;
+	};
 }
 
-#endif  // REVOLUTION_CLIENT_H
+#endif	// REVOLUTION_CLIENT_H
