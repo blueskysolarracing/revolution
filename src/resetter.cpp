2fda3979e7dbf1d64934db8c7c4a437015422e85
Juho Kim <juho-kim@outlook.com>
2022-11-15 17:37:48 -0500

    Allow unlinking mqueue with arguments

--- src/resetter.cpp
+++ src/resetter.cpp
@@ -1,12 +1,25 @@
+#include <string>
+#include <vector>
+
 #include <mqueue.h>
 
 #include "configuration.h"
 
-int main() {
-	Revolution::Topology topology;
+int main(int argc, char *argv[]) {
+	std::vector<std::string> names;
 
-	for (const auto& endpoint : topology.get_endpoints())
-		mq_unlink(('/' + endpoint.get().get_name()).data());
+	if (argc > 1)
+		for (int i = 0; i < argc; ++i)
+			names.emplace_back(argv[i]);
+	else {
+		Revolution::Topology topology;
+
+		for (const auto& endpoint : topology.get_endpoints())
+			names.push_back(endpoint.get().get_name());
+	}
+
+	for (const auto& name : names)
+		mq_unlink(('/' + name).data());
 
 	return 0;
 }
