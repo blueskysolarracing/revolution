23e0060ef58522dc6339bfa67ae50dfe11641366
Juho Kim <juho-kim@outlook.com>
2022-11-15 18:42:41 -0500

    Add pure listening feature to client and rename resetter to more aptly named unlinker

--- src/client.cpp
+++ src/client.cpp
@@ -9,7 +9,7 @@
 
 #include "messenger.h"
 
-void monitor(
+void receiver(
 	const std::string& recipient_name,
 	const Revolution::Messenger& messenger,
 	const std::atomic_bool& status
@@ -26,50 +26,21 @@ void monitor(
 	}
 }
 
-int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		std::cout << "Usage: "
-			<< "./client "
-			<< "sender_name "
-			<< "recipient_name "
-			<< "[header data...]"
-			<< std::endl;
-
-		return 0;
-	}
-
-	std::string sender_name{argv[1]};
-	std::string recipient_name{argv[2]};
+void sender(
+	const std::string& sender_name,
+	const std::string& recipient_name,
+	const Revolution::Messenger& messenger,
+	std::atomic_bool& status
+) {
 	std::string header;
 	std::vector<std::string> data;
-	Revolution::Messenger messenger;
-	std::atomic_bool status{true};
-
-	if (argc > 3) {
-		header = argv[3];
-
-		for (int i{4}; i < argc; ++i)
-			data.emplace_back(argv[i]);
-
-		messenger.send(
-			Revolution::Messenger::Message{
-				sender_name,
-				recipient_name,
-				header,
-				data
-			}
-		);
-		status.store(false);
-	}
-
-	std::thread thread{monitor, sender_name, messenger, std::cref(status)};
 
 	while (status.load()) {
 		std::string input;
 		std::getline(std::cin, input);
 
 		if (input.empty())
-			status.store(false);
+			status = false;
 		else {
 			std::istringstream iss(input);
 			std::string datum;
@@ -92,8 +63,63 @@ int main(int argc, char *argv[]) {
 			data.clear();
 		}
 	}
+}
 
-	thread.join();
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::cout << "Usage: "
+			<< "./client "
+			<< "sender_name "
+			<< "[recipient_name header data...]"
+			<< std::endl;
+
+		return -1;
+	}
+	
+	std::string sender_name{argv[1]};
+	Revolution::Messenger messenger;
+	std::atomic_bool status;
+
+	if (argc == 2) {
+		receiver(sender_name, messenger, status);
+
+		return 0;
+	} else if (argc == 3)
+		status = true;
+
+	std::string recipient_name{argv[2]};
+
+	if (argc > 3) {
+		std::string header{argv[3]};
+		std::vector<std::string> data{std::next(argv, 4), std::next(argv, argc)};
+
+		messenger.send(
+			Revolution::Messenger::Message{
+				sender_name,
+				recipient_name,
+				header,
+				data
+			}
+		);
+	}
+
+	std::thread receiver_thread{
+		receiver,
+		sender_name,
+		messenger,
+		std::cref(status)
+	};
+	std::thread sender_thread{
+		sender,
+		sender_name,
+		recipient_name,
+		messenger,
+		std::ref(status)
+	};
+
+	receiver_thread.join();
+	sender_thread.join();
 
 	return 0;
 }
