1c603bd49ba0700ebfc61e0f7d1092da6d8f9d2a
Juho Kim <juho-kim@outlook.com>
2022-12-12 01:53:46 -0500

    Add boilerplate code for new hw controllers.

--- src/controller.cpp
+++ src/controller.cpp
@@ -2,176 +2,82 @@
 
 #include <functional>
 #include <string>
-#include <vector>
-
-#include "application.h"
-#include "configuration.h"
 
 namespace Revolution {
-	Controller::Controller(
-		const std::reference_wrapper<const Header_space>& header_space,
-		const std::reference_wrapper<const State_space>& state_space,
-		const std::reference_wrapper<const Topology>& topology
-	) : Application{
-		header_space,
-		state_space,
-		topology,
-		topology.get().get_controller()
-	    } {}
+	// TODO: https://developer.toradex.com/linux-bsp/application-development/peripheral-access/gpio-linux
+	Controller::Controller(const std::string& device) : device{device} {}
 
-	void Controller::setup() {
-		Application::setup();
-
-		set_handler(
-			get_header_space().get_gpio(),
-			std::bind(
-				&Controller::handle_gpio,
-				this,
-				std::placeholders::_1
-			)
-		);
-		set_handler(
-			get_header_space().get_pwm(),
-			std::bind(
-				&Controller::handle_pwm,
-				this,
-				std::placeholders::_1
-			)
-		);
-		set_handler(
-			get_header_space().get_spi(),
-			std::bind(
-				&Controller::handle_spi,
-				this,
-				std::placeholders::_1
-			)
-		);
-		set_handler(
-			get_header_space().get_uart(),
-			std::bind(
-				&Controller::handle_uart,
-				this,
-				std::placeholders::_1
-			)
-		);
-
-		// TODO: https://github.com/toradex/torizon-samples/blob/bullseye/gpio/c/gpio-event.c
-		// TODO: https://developer.toradex.com/linux-bsp/application-development/peripheral-access/gpio-linux
+	const std::string& Controller::get_device() const {
+		return device;
 	}
 
-	std::vector<std::string>
-		Controller::handle_gpio(const Messenger::Message& message) {
+	GPIO::GPIO(const std::string& device, const unsigned int& offset)
+		: Controller{device}, offset{offset} {}
+
+	const unsigned int& GPIO::get_offset() const {
+		return offset;
+	}
+
+	bool GPIO::get_active_low() const {
 		// TODO: https://github.com/toradex/torizon-samples/blob/bullseye/gpio/c/gpio-toggle.c
-		// TODO: https://developer.toradex.com/linux-bsp/application-development/peripheral-access/gpio-linux
-
-		if (message.get_data().size() == 2) {
-			// const auto& device = message.get_data().front();
-			// const auto& raw_offset = message.get_data().back();
-			// unsigned int offset = (unsigned int) std::stoi(raw_offset);
-			
-			// TODO
-		} else if (message.get_data().size() == 3) {
-			// const auto& device = message.get_data().front();
-			// const auto& raw_offset = message.get_data()[1];
-			// const auto& raw_active_low = message.get_data().back();
-			// unsigned int offset = (unsigned int) std::stoi(raw_offset);
-			// bool active_low = (bool) std::stoi(raw_active_low);
-
-			// TODO
-		}
-
-		get_logger() << Logger::Severity::error
-			<< "Gpio expects 2 or 3 arguments, but "
-			<< message.get_data().size()
-			<< " argument(s) were supplied. "
-			<< "This message will be ignored."
-			<< std::endl;
-
-		return {};
+		
+		return false;
 	}
 
-	std::vector<std::string>
-		Controller::handle_pwm(const Messenger::Message& message) {
+	void GPIO::set_active_low(const bool& active_low) const {
+		// TODO: https://github.com/toradex/torizon-samples/blob/bullseye/gpio/c/gpio-toggle.c
+	}
+
+	void GPIO::monitor(
+		const std::function<
+			void(
+				const std::string&,
+				const unsigned int&,
+				const bool&
+			)
+		>& handler
+	) const {
+		// TODO: https://github.com/toradex/torizon-samples/blob/bullseye/gpio/c/gpio-event.c
+	}
+
+	const std::string PWM::Polarity::normal{"normal"};
+	const std::string PWM::Polarity::inversed{"inversed"};
+
+	void PWM::enable(
+		const unsigned int& period,
+		const unsigned int& duty_cycle,
+		const Polarity& polarity
+	) const {
 		// TODO: https://github.com/toradex/torizon-samples/tree/bullseye/pwm
-
-		if (message.get_data().size() == 1) {
-			// const auto& device = message.get_data().front();
-			
-			// TODO
-		} else if (message.get_data().size() == 2) {
-			// const auto& device = message.get_data().front();
-
-			// TODO
-		}
-
-		get_logger() << Logger::Severity::error
-			<< "Pwm expects 1 or 2 arguments, but "
-			<< message.get_data().size()
-			<< " argument(s) were supplied. "
-			<< "This message will be ignored."
-			<< std::endl;
-
-		return {};
 	}
 
-	std::vector<std::string>
-		Controller::handle_spi(const Messenger::Message& message) {
+	void PWM::disable() const {
+		// TODO: https://github.com/toradex/torizon-samples/tree/bullseye/pwm
+	}
+
+	void SPI::transmit(const std::string& data) const {
+		// TODO: https://github.com/torvalds/linux/blob/v5.15/tools/spi/spidev_test.c
+	}
+
+	std::string SPI::receive() const {
 		// TODO: https://github.com/torvalds/linux/blob/v5.15/tools/spi/spidev_test.c
 
-		if (message.get_data().size() == 1) {
-			// const auto& device = message.get_data().front();
-
-			// TODO
-		} else if (message.get_data().size() == 2) {
-			// const auto& device = message.get_data().front();
-			// const auto& tx = message.get_data().back();
-
-			// TODO
-		}
-
-		get_logger() << Logger::Severity::error
-			<< "Spi expects 1 or 2 arguments, but "
-			<< message.get_data().size()
-			<< " argument(s) were supplied. "
-			<< "This message will be ignored."
-			<< std::endl;
-
-		return {};
+		return "";
 	}
 
-	std::vector<std::string>
-		Controller::handle_uart(const Messenger::Message& message) {
+	std::string SPI::transmit_and_receive(const std::string& data) const {
+		// TODO: https://github.com/torvalds/linux/blob/v5.15/tools/spi/spidev_test.c
+
+		return "";
+	}
+
+	void UART::transmit(const std::string& data) const {
+		// TODO: https://developer.toradex.com/linux-bsp/application-development/peripheral-access/uart-linux#boards
+	}
+
+	std::string UART::receive() const {
 		// TODO: https://developer.toradex.com/linux-bsp/application-development/peripheral-access/uart-linux#boards
 
-		if (message.get_data().size() == 1) {
-			// const auto& device = message.get_data().front();
-			
-			// TODO
-		} else if (message.get_data().size() == 2) {
-			// const auto& device = message.get_data().front();
-			// const auto& tx = message.get_data().back();
-
-			// TODO
-		}
-
-		get_logger() << Logger::Severity::error
-			<< "Uart expects 1 or 2 arguments, but "
-			<< message.get_data().size()
-			<< " argument(s) were supplied. "
-			<< "This message will be ignored."
-			<< std::endl;
-
-		return {};
+		return "";
 	}
-}
-
-int main() {
-	Revolution::Header_space header_space;
-	Revolution::State_space state_space;
-	Revolution::Topology topology;
-	Revolution::Controller controller{header_space, state_space, topology};
-
-	controller.main();
-
-	return 0;
 }
--- src/controller.h
+++ src/controller.h
@@ -3,32 +3,74 @@
 
 #include <functional>
 #include <string>
-#include <vector>
-
-#include "application.h"
-#include "configuration.h"
 
 namespace Revolution {
-	class Controller : public Application {
+	class Controller {
 	public:
-		explicit Controller(
-			const std::reference_wrapper<const Header_space>&
-				header_space,
-			const std::reference_wrapper<const State_space>&
-				state_space,
-			const std::reference_wrapper<const Topology>& topology
-		);
-	protected:
-		void setup() override;
+		explicit Controller(const std::string& device);
+
+		const std::string& get_device() const;
 	private:
-		std::vector<std::string>
-			handle_gpio(const Messenger::Message& message);
-		std::vector<std::string>
-			handle_pwm(const Messenger::Message& message);
-		std::vector<std::string>
-			handle_spi(const Messenger::Message& message);
-		std::vector<std::string>
-			handle_uart(const Messenger::Message& message);
+		const std::string device;
+	};
+
+	class GPIO : public Controller {
+	public:
+		explicit GPIO(
+			const std::string& device,
+			const unsigned int& offset
+		);
+
+		const unsigned int& get_offset() const;
+
+		bool get_active_low() const;
+		void set_active_low(const bool& active_low) const;
+		void monitor(
+			const std::function<
+				void(
+					const std::string&,
+					const unsigned int&,
+					const bool&
+				)
+			>& handler
+		) const;
+	private:
+		const unsigned int offset;
+	};
+
+	class PWM : public Controller {
+	public:
+		using Controller::Controller;
+
+		class Polarity {
+		public:
+			static const std::string normal;
+			static const std::string inversed;
+		};
+
+		void enable(
+			const unsigned int& period,
+			const unsigned int& duty_cycle,
+			const Polarity& polarity
+		) const;
+		void disable() const;
+	};
+
+	class SPI : public Controller {
+	public:
+		using Controller::Controller;
+
+		void transmit(const std::string& data) const;
+		std::string receive() const;
+		std::string transmit_and_receive(const std::string& data) const;
+	};
+
+	class UART : public Controller {
+	public:
+		using Controller::Controller;
+
+		void transmit(const std::string& data) const;
+		std::string receive() const;
 	};
 }
 
