c8e6aa2d314d326c8a7d88ed139e34c0e985bb29
Tristan <robitaille.tristan@gmail.com>
2023-03-05 20:22:15 -0800

    Snake-cased, fixed channel indexing in run10ms, improved type defs

--- revolution/drivers/ADC78H89.py
+++ revolution/drivers/ADC78H89.py
@@ -8,7 +8,7 @@
     -Each conversion requires 16 CLK cycle, and is triggered when !CS goes low. The conversion is applied to the channel in the Control Register.
      Therefore, there is a one sample delay between writing to the control register and reading the data.
     -Write to the Control Register to select which channel is being sampled: [X X ADD2 ADD1 ADD0 X X X]. Even though this register is 8b, writing to it requires 16 clock cycles since a conversion is taking place in parallel. Only the first 8b are read. 
-    -First conversion after power-up is channel #1
+    -First conversion after power-up is channel #1 (0x00)
 
     This wrapper also takes in pins that might not to be demuxed to generate the CS signal as a list of "GPIOsConfig". The GPIOs are set to setPolarity before SPI transfer and reset to resetPolarity after.
     CS_GPIOsConfig =   [[GPIO1_devpath, GPIO1_line, GPIO1_setPolarity, GPIO1_resetPolarity], 
@@ -29,34 +29,35 @@ REF_VOLTAGE = 3.3
 
 ############## CLASSES ##############
 class channel(Enum):
-    CHANNEL_1 = 1
-    CHANNEL_2 = 2
-    CHANNEL_3 = 3
-    CHANNEL_4 = 4
-    CHANNEL_5 = 5
-    CHANNEL_6 = 6
-    CHANNEL_7 = 7
+    CHANNEL_1 = 0
+    CHANNEL_2 = 1
+    CHANNEL_3 = 2
+    CHANNEL_4 = 3
+    CHANNEL_5 = 4
+    CHANNEL_6 = 5
+    CHANNEL_7 = 6
 
 class ADC78H89():
     def __init__(self, devpath:str) -> None:
-        self.__voltageBuffer = len(channel)*[-1.0]
+        self.__voltageBuffer: list[float] = len(channel)*[-1.0]
         self.__spiDev = SPI(devpath, SPI_MODE, SPI_MAX_SPEED, SPI_BIT_ORDER, SPI_BITS_PER_WORDS)
 
     """
         Returns the last unfiltered voltage
     """
-    def getMeasurementRaw(self, ch:Enum) -> float:
-        return self.__voltageBuffer[ch.value]
+    def get_measurement_raw(self, ch:Enum) -> float:
+        voltage: float = self.__voltageBuffer[ch.value]
+        return voltage
 
     """
         Handles the SPI calls and updates the voltage buffer. This method is expected to be called every 10ms
     """
-    def run10ms(self) -> None:
+    def run_10ms(self) -> None:
         for ch in channel:
-            nextChannel = ch.value % 7 #Rollover to 0 at last channel
+            nextChannel = (ch.value + 1) % 7 #Rollover to 0 at last channel
             controlRegister = [(nextChannel << 5), 0x00] #TODO: Need to double-check order that this is sent
 
             rawData = self.__spiDev.transfer(controlRegister)
 
-            ADCCode = (rawData[0] << 8) + rawData[1] #TODO: Need to double-check order that this is sent
+            ADCCode = (rawData[0] << 8) + rawData[1] #TODO: Need to double-check order that this is received
             self.__voltageBuffer[ch.value] = REF_VOLTAGE * (ADCCode / 4096.0)
