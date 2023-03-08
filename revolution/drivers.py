9f98f6d1933457afb3d96b471d6d037ac2b05a01
Juho Kim <juho-kim@outlook.com>
2023-03-08 17:13:46 -0500

    Cleanup ADC class

--- revolution/drivers.py
+++ revolution/drivers.py
@@ -1,63 +1,107 @@
-"""
-    Wrapper for the Texas Instruments ADC78H89 SPI ADC
-    Datasheet: https://www.ti.com/lit/ds/symlink/adc78h89.pdf?ts=1677212870525&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FADC78H89 
+from dataclasses import dataclass
+from enum import IntEnum
+from periphery import SPI
+from typing import ClassVar
+from warnings import warn
 
-    ADC78H89 7-Channel, 500 KSPS, 12-Bit A/D Converter
-    -Max. conversion rate: 500KSPS 
-    -Data is straight binary, refer to Fig. 23 
-    -Each conversion requires 16 CLK cycle, and is triggered when !CS goes low. The conversion is applied to the channel in the Control Register.
-     Therefore, there is a one sample delay between writing to the control register and reading the data.
-    -Write to the Control Register to select which channel is being sampled: [X X ADD2 ADD1 ADD0 X X X]. Even though this register is 8b, writing to it requires 16 clock cycles since a conversion is taking place in parallel. Only the first 8b are read. 
-    -First conversion after power-up is channel #1 (0x00)
 
-    This wrapper also takes in pins that might not to be demuxed to generate the CS signal as a list of "GPIOsConfig". The GPIOs are set to setPolarity before SPI transfer and reset to resetPolarity after.
-    CS_GPIOsConfig =   [[GPIO1_devpath, GPIO1_line, GPIO1_setPolarity, GPIO1_resetPolarity], 
-                        [GPIO2_devpath, GPIO2_line, GPIO2_setPolarity, GPIO2_resetPolarity], 
-                        [GPIO3_devpath, GPIO3_line, GPIO3_setPolarity, GPIO3_resetPolarity], ...]
-"""
+@dataclass
+class ADC78H89:
+    """The wrapper class for Texas Instruments ADC78H89 7-Channel, 500
+    KSPS, 12-Bit A/D Converter.
 
-from enum import Enum
-from periphery.spi import SPI
-from typing import List, Generator
+    Datasheet for this device is included in the documentation of
+    ``revolution``.
 
-############# CONSTANTS #############
-SPI_MODE = 3
-SPI_MAX_SPEED = 1000000 #Supported SPI frequency is 500kHz to 8MHz. Each conversion takes 16/1000000 = 16us.
-SPI_BIT_ORDER = 'msb'
-SPI_BITS_PER_WORDS = 8
-REF_VOLTAGE = 3.3
-
-############## CLASSES ##############
-class channel(Enum):
-    CHANNEL_1 = 0
-    CHANNEL_2 = 1
-    CHANNEL_3 = 2
-    CHANNEL_4 = 3
-    CHANNEL_5 = 4
-    CHANNEL_6 = 5
-    CHANNEL_7 = 6
-
-class ADC78H89():
-    def __init__(self, devpath:str) -> None:
-        self.__voltageBuffer: list[float] = len(channel)*[-1.0]
-        self.__spiDev = SPI(devpath, SPI_MODE, SPI_MAX_SPEED, SPI_BIT_ORDER, SPI_BITS_PER_WORDS)
-
+    Summary of specifications:
+    - Maximum conversion rate is 500 KSPS.
+    - Data is straight binary, refer to Fig. 23.
+    - Each conversion requires 16 CLK cycle, and is triggered when !CS
+      goes low. The conversion is applied to the channel in the Control
+      Register. Therefore, there is a one sample delay between writing
+      to the control register and reading the data.
+    - Write to the Control Register to select which channel is being
+      sampled: [X X ADD2 ADD1 ADD0 X X X].
+    - Even though this register is 8 bytes, writing to it requires 16
+      clock cycles since a conversion is taking place in parallel. Only
+      the first 8 bytes are read.
+    - First conversion after power-up is for Channel #1 (0x00).
     """
-        Returns the last unfiltered voltage
-    """
-    def get_measurement_raw(self, ch:Enum) -> float:
-        voltage: float = self.__voltageBuffer[ch.value]
-        return voltage
 
-    """
-        Handles the SPI calls and updates the voltage buffer. This method is expected to be called every 10ms
-    """
-    def run_10ms(self) -> None:
-        for ch in channel:
-            nextChannel = (ch.value + 1) % 7 #Rollover to 0 at last channel
-            controlRegister = [(nextChannel << 5), 0x00] #TODO: Need to double-check order that this is sent
+    class InputChannel(IntEnum):
+        """The enum class for input channels."""
 
-            rawData = self.__spiDev.transfer(controlRegister)
+        AIN1: int = 0
+        """The first (default) input channel."""
+        AIN2: int = 1
+        """The second input channel."""
+        AIN3: int = 2
+        """The third input channel."""
+        AIN4: int = 3
+        """The fourth input channel."""
+        AIN5: int = 4
+        """The fifth input channel."""
+        AIN6: int = 5
+        """The fifth input channel."""
+        AIN7: int = 6
+        """The seventh input channel."""
+        GROUND: int = 7
+        """The ground input channel."""
 
-            ADCCode = (rawData[0] << 8) + rawData[1] #TODO: Need to double-check order that this is received
-            self.__voltageBuffer[ch.value] = REF_VOLTAGE * (ADCCode / 4096.0)
+    spi_mode: ClassVar[int] = 3
+    """The supported spi mode."""
+    min_spi_max_speed: ClassVar[float] = 5e5
+    """The supported minimum spi maximum speed."""
+    max_spi_max_speed: ClassVar[float] = 8e6
+    """The supported maximum spi maximum speed."""
+    spi_bit_order: ClassVar[str] = 'msb'
+    """The supported spi bit order."""
+    spi_word_bit_count: ClassVar[int] = 8
+    """The supported spi number of bits per word."""
+    offset: ClassVar[int] = 3
+    """The input channel bit offset for control register bits."""
+    reference_voltage: ClassVar[float] = 3.3
+    """The reference voltage value (in volts)."""
+    divisor: ClassVar[float] = 4096
+    """The lsb width for ADC78H89."""
+    spi: SPI
+    """The SPI for the ADC device."""
+
+    def __post_init__(self) -> None:
+        if self.spi.mode != self.spi_mode:
+            raise ValueError('unsupported spi mode')
+        elif not self.min_spi_max_speed <= self.spi.max_speed \
+                <= self.max_spi_max_speed:
+            raise ValueError('unsupported spi maximum speed')
+        elif self.spi.bit_order != self.spi_bit_order:
+            raise ValueError('unsupported spi bit order')
+        elif self.spi.bits_per_word != self.spi_word_bit_count:
+            raise ValueError('unsupported spi number of bits per word')
+
+        if self.spi.extra_flags:
+            warn('unknown spi extra flags')
+
+    @property
+    def voltages(self) -> dict[InputChannel, float]:
+        """Handle the SPI calls and return the last unfiltered voltage
+        for the specified input channel.
+
+        :param input_channel: The input channel to query.
+        :return: The last unfiltered voltage for the specified input
+                 channel.
+        """
+        voltages = {}
+
+        for input_channel in self.InputChannel:
+            next_input_channel = (input_channel + 1) % len(self.InputChannel)
+            sent_data = [(next_input_channel << self.offset), 0]
+            received_data = self.spi.transfer(sent_data)
+
+            assert len(received_data) == 2
+
+            raw_data = received_data[0] << self.spi_word_bit_count \
+                | received_data[1]
+            voltages[input_channel] \
+                = self.reference_voltage * raw_data / self.divisor
+
+        return voltages
