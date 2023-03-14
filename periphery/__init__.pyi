37ad21afa5d03fed7fb3a866579c2f186a49f56b
Juho Kim <juho-kim@outlook.com>
2023-03-14 17:02:41 -0400

    Add type stubs for python-periphery

--- periphery/__init__.pyi
+++ periphery/__init__.pyi
@@ -5,11 +5,11 @@ from periphery.gpio import (
     GPIOError as GPIOError,
     SysfsGPIO as SysfsGPIO,
 )
-# from periphery.i2c import I2C as I2C, I2CError as I2CError
-# from periphery.led import LED as LED, LEDError as LEDError
-# from periphery.mmio import MMIO as MMIO, MMIOError as MMIOError
+from periphery.i2c import I2C as I2C, I2CError as I2CError
+from periphery.led import LED as LED, LEDError as LEDError
+from periphery.mmio import MMIO as MMIO, MMIOError as MMIOError
 from periphery.pwm import PWM as PWM, PWMError as PWMError
-# from periphery.serial import Serial as Serial, SerialError as SerialError
+from periphery.serial import Serial as Serial, SerialError as SerialError
 from periphery.spi import SPI as SPI, SPIError as SPIError
 
 version: tuple[int, int, int]
