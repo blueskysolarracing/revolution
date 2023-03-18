bf38fd63ffa19868e505c0acfcc5e28ca1986196
Emma Fu <emmafu301@hotmail.com>
2023-03-18 15:28:04 -0400

    Added LTC code into driver, add battery/bms module

---
+++ revolution/battery.py
@@ -0,0 +1,99 @@
+from multiprocessing import get_logger
+from queue import Queue
+from enum import Enum, auto
+from revolution.drivers import LTC6810
+
+
+_logger = get_logger()
+
+############# CONSTANTS FOR BMS #############
+BATTERY_NUM_CELLS = 5
+BATTERY_NUM_VOLTAGES = BATTERY_NUM_CELLS
+BATTERY_NUM_STATE_OF_CHARGES = BATTERY_NUM_CELLS
+
+BATTERY_NUM_TEMPERATURE_SENSOR = 3
+BATTERY_NUM_TEMPERATURES = BATTERY_NUM_TEMPERATURE_SENSOR
+
+
+def init_queue_from_arr(input_arr: list[float]) -> Queue[float]:
+    queue: Queue[float] = Queue()
+    for element in input_arr:
+        queue.put(element)
+    return queue
+
+
+def get_queue_average(queue: Queue[float]) -> float:
+    total = 0.0
+    for element in queue.queue:
+        total += element
+    return 0.0 if queue.qsize() == 0 else total / queue.qsize()
+
+
+class Battery:
+    class GetMode(Enum):
+        GET_MOST_RECENT = auto()
+        GET_PAST_AVERAGE = auto()
+        GET_FILTERED_RESULT = auto()
+
+    def __init__(self, battery_id: int, spi, gpio_b) -> None:
+        self.ltc6810 = LTC6810(spi=spi, gpio_b=gpio_b)
+
+        self.battery_id = battery_id
+        self._current = 0.0
+        self._state_of_charge_arr = [0.0] * BATTERY_NUM_STATE_OF_CHARGES
+
+        self._voltage_arr = self.ltc6810.read_voltage()  # voltage of each cell
+        self._past_voltage_queue = init_queue_from_arr(self._voltage_arr)
+        self._temperature_arr = self.ltc6810.read_temp(0, 0, 0, 0, 0)
+        self._past_temp_queue = init_queue_from_arr(self._temperature_arr)
+
+        # TODO: initialize battery algo using EKF
+
+    def get_temperature(self, temperatures: list[float], get_mode: GetMode) -> None:
+
+        for i in range(BATTERY_NUM_TEMPERATURES):
+            if get_mode == Battery.GetMode.GET_MOST_RECENT:
+                temperatures[i] = self._temperature_arr[i]
+            elif get_mode == Battery.GetMode.GET_PAST_AVERAGE:
+                temperatures[i] = get_queue_average(self._past_temp_queue)
+            elif get_mode == Battery.GetMode.GET_FILTERED_RESULT:
+                pass
+
+    def get_voltage(self, voltages: list[float], get_mode: GetMode) -> None:
+
+        for i in range(BATTERY_NUM_VOLTAGES):
+            if get_mode == Battery.GetMode.GET_MOST_RECENT:
+                voltages[i] = self._voltage_arr[i]
+            elif get_mode == Battery.GetMode.GET_PAST_AVERAGE:
+                voltages[i] = get_queue_average(self._past_voltage_queue)
+            elif get_mode == Battery.GetMode.GET_FILTERED_RESULT:
+                pass
+
+    def get_state_of_charge(self, state_of_charges: list[float]) -> None:
+        for i in range(BATTERY_NUM_STATE_OF_CHARGES):
+            state_of_charges[i] = self._state_of_charge_arr[i]
+
+    def measure_temperature(self) -> None:
+        # Note: the following function reads voltage
+        local_temp_arr = self.ltc6810.read_temp(0, 0, 0, 0, 0)
+        local_temp_arr = self.ltc6810.voltage_to_temp(local_temp_arr)
+        for i in range(BATTERY_NUM_TEMPERATURES):
+            self._temperature_arr[i] = local_temp_arr[i]
+            self._past_temp_queue.put(local_temp_arr[i])
+
+    def measure_voltage(self) -> None:
+        local_voltage_arr = self.ltc6810.read_voltage()
+        for i in range(BATTERY_NUM_VOLTAGES):
+            self._voltage_arr[i] = local_voltage_arr[i]
+            self._past_voltage_queue.put(local_voltage_arr[i])
+
+    def compute_soc(self) -> None:
+        local_soc_arr = [0.0] * BATTERY_NUM_STATE_OF_CHARGES
+
+        # TODO: compute algorithm with EKF_models
+
+        for i in range(BATTERY_NUM_STATE_OF_CHARGES):
+            self._state_of_charge_arr[i] = local_soc_arr[i]
+
+    def set_current(self, current: float) -> None:
+        self._current = current
--- revolution/drivers.py
+++ revolution/drivers.py
@@ -1,14 +1,17 @@
+import math
 from dataclasses import dataclass
 from enum import IntEnum
 from logging import getLogger
 from math import inf
 from time import sleep, time
-from typing import ClassVar
+from typing import ClassVar, Optional
 from warnings import warn
 
 from periphery import GPIO, SPI
 
 from revolution.environment import Direction
+from revolution.battery import BATTERY_NUM_TEMPERATURES, BATTERY_NUM_VOLTAGES
+
 
 _logger = getLogger(__name__)
 
@@ -209,3 +212,343 @@ class ADC78H89:
                 = self.reference_voltage * raw_data / self.divisor
 
         return voltages
+
+
+@dataclass
+class LTC6810:
+    spi_mode: ClassVar[int] = 3
+    """The supported spi mode."""
+    max_spi_max_speed: ClassVar[float] = 1e6    # TODO: read document
+    """The supported maximum spi maximum speed."""
+    spi_bit_order: ClassVar[str] = 'msb'
+    """The supported spi bit order."""
+    spi_word_bit_count: ClassVar[int] = 8
+    """The supported spi number of bits per word."""
+    reference_voltage: ClassVar[float] = 3.3
+    """The reference voltage value (in volts)."""
+    spi: SPI
+    """The SPI for the device."""
+    gpio_b: GPIO
+    """The GPIO for the device."""
+
+    def __post_init__(self) -> None:
+        if self.spi.mode != self.spi_mode:
+            raise ValueError('unsupported spi mode')
+        elif not self.spi.max_speed <= self.max_spi_max_speed:
+            raise ValueError('unsupported spi maximum speed')
+        elif self.spi.bit_order != self.spi_bit_order:
+            raise ValueError('unsupported spi bit order')
+        elif self.spi.bits_per_word != self.spi_word_bit_count:
+            raise ValueError('unsupported spi number of bits per word')
+
+        if self.spi.extra_flags:
+            warn('unknown spi extra flags')
+
+    def initialize_board(self, gpio_4: int, gpio_3: int, gpio_2: int, dcc_5: int, dcc_4: int, dcc_3: int, dcc_2: int, dcc_1: int) -> None:
+        """
+        Initialize the LTC6810 ADC chip
+        To modify the initialization setting, please refer to the Datasheet and the chart below
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | reg   | Bit7    | Bit6    | Bit5    | Bit4    | Bit3    | Bit2    | Bit1    | Bit0   |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR0 | RSVD    | GPIO4   | GPIO3   | GPIO2   | GPIO1   | REFON   | DTEN    | ADCOPT |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR1 | VUV[7]  | VUV[6]  | VUV[5]  | VUV[4]  | VUV[3]  | VUV[2]  | VUV[1]  | VUV[0] |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR2 | VOV[3]  | VOV[2]  | VOV[1]  | VOV[0]  | VUV[11] | VUV[10] | VUV[9]  | VUV[8] |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR3 | VOV[11] | VOV[10] | VOV[9]  | VOV[8]  | VOV[7]  | VOV[6]  | VOV[5]  | VOV[4] |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR4 | DCC0    | MCAL    | DCC6    | DCC5    | DCC4    | DCC3    | DCC2    | DCC1   |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         | CFGR5 | DCTO[3] | DCTO[2] | DCTO[1] | DCTO[0] | SCONV   | FDRF    | DIS_RED | DTMEN  |
+         +-------+---------+---------+---------+---------+---------+---------+---------+--------+
+         Connection to MUX for thermal measurements:
+         GPIO1 -> A0
+         GPIO3 -> A1
+         GPIO4 -> A2
+         GPIO1 shall always be set to 1 to avoid internal pull-down, as its the voltage reading pin.
+         A2 is MSB and A0 is MSB, if want channel 2 -> do A2=0, A1=1, A0=0
+        """
+        data_to_send = LTC6810.generate_data_to_send(gpio_4, gpio_3, gpio_2, dcc_5, dcc_4, dcc_3, dcc_2, dcc_1)
+        self.spi.transfer(data_to_send)
+
+    @staticmethod
+    def generate_data_to_send(gpio_4: int, gpio_3: int, gpio_2: int, dcc_5: int, dcc_4: int, dcc_3: int, dcc_2: int, dcc_1: int) -> list[int]:
+        """
+        Generate the data to send to the LTC6810 chip based on pin configuration
+        :return: array of data to send to the chip
+        """
+        data_6_byte = [0] * 48  # [47] is MSB, CFGR0 Bit7
+        # stores command and PEC bit of WRCFG [2 byte command, 2 byte PEC, 6 byte data, 2 byte PEC]
+        data_to_send = [0] * 12
+
+        # write configure command
+        message_in_binary = 0b1
+        LTC6810.generate_command(message_in_binary, data_to_send)
+
+        '''
+        Set GPIO bits to 1 so they aren`t being pulled down internally by the chip
+        Set REFON to enable the 3V that goes to the chip
+        DTEN to 0 to disable discharge timer
+        ADCOPT bit to 0, use 422Hz as its stable
+        Above are byte0, byte 1 full of 0s as VUV currently not used
+        '''
+        message_in_binary = 0b0000110000000000  # CFGR0&1
+        message_in_binary += 4096 * gpio_2 + 8192 * gpio_3 + 16384 * gpio_4
+
+        msg_0, msg_1 = LTC6810.command_to_array(message_in_binary)
+        for i in range(8):
+            data_6_byte[40 + i] = msg_0[i]
+            data_6_byte[32 + i] = msg_1[i]
+
+        # Set the VOV and VUV as 0
+        message_in_binary = 0b0000000000000000  # CFGR2&3
+        msg_2, msg_3 = LTC6810.command_to_array(message_in_binary)
+        for i in range(8):
+            data_6_byte[24 + i] = msg_2[i]
+            data_6_byte[16 + i] = msg_3[i]
+
+        '''
+        DCC = 0: discharge off, 1: discharge = on
+        MCAL = 1: enable multi-calibration. for this don`t use, turn to 0
+        DCTO: discharge timer. for now 0
+        SCONV: redundant measurement using S pin, disable for now (0)
+        FDRF: not using it anyway, to 0
+        DIS_RED: redundancy disable: set to 1 to disable
+        DTMEN: Discharge timer monitor, 0 to disable
+        '''
+        message_in_binary = 0b0000000000000010
+        message_in_binary += 256 * dcc_1 + 512 * dcc_2 + 1024 * dcc_3 + 2048 * dcc_4 + 4096 * dcc_5
+        msg_4, msg_5 = LTC6810.command_to_array(message_in_binary)
+        for i in range(8):
+            data_6_byte[8 + i] = msg_4[i]
+            data_6_byte[0 + i] = msg_5[i]
+
+        # generate PEC bits based on above data
+        pec0_6bytes, pec1_6bytes = LTC6810.generate_pec_bits(True, data_6_byte)
+
+        # change array back to bytes
+        data_to_send[4] = LTC6810.array_to_byte(msg_0)
+        data_to_send[5] = LTC6810.array_to_byte(msg_1)
+        data_to_send[6] = LTC6810.array_to_byte(msg_2)
+        data_to_send[7] = LTC6810.array_to_byte(msg_3)
+        data_to_send[8] = LTC6810.array_to_byte(msg_4)
+        data_to_send[9] = LTC6810.array_to_byte(msg_5)
+        data_to_send[10] = LTC6810.array_to_byte(pec0_6bytes)
+        data_to_send[11] = LTC6810.array_to_byte(pec1_6bytes)
+
+        return data_to_send
+
+    @staticmethod
+    # combined LTC6810GeneratePECbits and LTC6810GeneratePECbits6Byte from GEN11
+    def generate_pec_bits(is_6_bytes: bool, data: list[int], cmd1: Optional[list[int]] = None) -> tuple[list[int], list[int]]:
+        """
+        Generate PEC bits based on 6-byte data or commands
+        :param is_6_bytes: whether to generate PEC bits based on 6-byte data
+        :param data: 6-byte data or command array
+        :param cmd1: additional command array for non-6-byte data PEC generation
+        :return: None
+        """
+        pec_0 = [0] * 8
+        pec_1 = [0] * 8
+        # initialize the 15 bit pec. in last cycle shift up add 0 at LSB
+        pec = [0] * 15
+        pec[4] = 1
+
+        # note for 6 bytes data, data[47] is 1st bit
+        iterations = 47 if is_6_bytes else 15
+        for i in range(iterations, -1, -1):
+            # step 1: assign DIN value
+            if is_6_bytes:
+                din = data[i]
+            elif cmd1 is not None:
+                din = data[i - 8] if i >= 8 else cmd1[i]
+
+            # step 2: shifting
+            in0 = din ^ pec[14]
+            in3 = in0 ^ pec[2]
+            in4 = in0 ^ pec[3]
+            in7 = in0 ^ pec[6]
+            in8 = in0 ^ pec[7]
+            in10 = in0 ^ pec[9]
+            in14 = in0 ^ pec[13]
+
+            # step 3: shifting
+            pec[14] = in14
+            pec[13] = pec[12]
+            pec[12] = pec[11]
+            pec[11] = pec[10]
+            pec[10] = in10
+            pec[9] = pec[8]
+            pec[8] = in8
+            pec[7] = in7
+            pec[6] = pec[5]
+            pec[5] = pec[4]
+            pec[4] = in4
+            pec[3] = in3
+            pec[2] = pec[1]
+            pec[1] = pec[0]
+            pec[0] = in0
+
+        # adjusting pec_0 and pec_1
+        for j in range(14, -1, -1):
+            if j >= 7:
+                pec_0[j - 7] = pec[j]
+            else:
+                pec_1[j + 1] = pec[j]
+
+        # add 0 at the end to make it 2 bytes
+        pec_1[0] = 0
+        return pec_0, pec_1
+
+    @staticmethod
+    def array_to_byte(in_array: list[int]) -> int:
+        """
+        Converting array containing 8 bits to byte
+        :return: 8-bit unsigned integer
+        """
+        # note: [7] is MSB, [0] is LSB
+        bit_str = "".join(str(bit) for bit in in_array)
+        return int(bit_str, 2)
+
+    @staticmethod
+    def command_to_array(command: int) -> tuple[list[int], list[int]]:
+        """
+        Given command, convert into two binary arrays
+        :param command: message in binary
+        :return: two binary arrays
+        """
+        cmd0_ref = [0] * 8
+        cmd1_ref = [0] * 8
+        for i in range(16):  # from LSB to MSB, fill cmd1 first
+            temp_bit = command % 2
+            if i <= 7:
+                cmd1_ref[i] = temp_bit
+            else:
+                cmd0_ref[i - 8] = temp_bit
+
+            command = command >> 1  # right shift to remove LSB
+
+        return cmd0_ref, cmd1_ref
+
+    @staticmethod
+    def generate_command(command: int, data_to_send: list[int]):
+        """
+        Fill in data_to_send with command and error checking bits
+        :param command: command to convert
+        :param data_to_send: array of integer containing message and error check
+        :return: None
+        """
+        # convert command into two separate array
+        cmd0_list, cmd1_list = LTC6810.command_to_array(command)
+
+        # generate error checking bits
+        pec_bits_0, pec_bits_1 = LTC6810.generate_pec_bits(False, cmd0_list, cmd1_list)
+
+        # put data back into data_to_send list, with size of 4
+        data_to_send[0] = LTC6810.array_to_byte(cmd0_list)  # 1st byte message
+        data_to_send[1] = LTC6810.array_to_byte(cmd1_list)  # 2nd byte message
+        data_to_send[2] = LTC6810.array_to_byte(pec_bits_0)  # 1st byte error check
+        data_to_send[3] = LTC6810.array_to_byte(pec_bits_1)  # 2nd byte error check
+
+    @staticmethod
+    def data_to_voltage(low_byte: int, high_byte: int) -> int:
+        """
+        Calculate final voltage based on (total - 8192) * 100uV
+        Based on LTC6810 datasheet, every bit is 100uV
+        Full range of 16 bytes: -0.8192 to + 5.7344
+        :param low_byte: low byte of data (uint8_t)
+        :param high_byte: high byte of data (uint8_t)
+        :return: converted voltage
+        """
+        return low_byte + high_byte * 256
+
+    @staticmethod
+    def voltage_to_temp(input_voltage: list[float]) -> list[float]:
+        """
+        Convert raw ADC code to temperature
+        :param input_voltage: list of voltage values
+        :return: list of temperature values
+        """
+        output_temperature = [0.0] * 3
+        temp_correction_multiplier = 1.0
+        temp_correction_offset = 0.0
+        for i in range(BATTERY_NUM_TEMPERATURES):
+            corrected_voltage = temp_correction_multiplier * (input_voltage[i] / 10000.0 - temp_correction_offset)
+            thermistor_resistance = 10.0 / ((2.8 / corrected_voltage) - 1.0)
+            output_temperature[i] = 1.0 / (0.003356 + 0.0002532 * math.log(thermistor_resistance / 10.0))
+            output_temperature[i] = output_temperature[i] - 273.15
+
+            # Preventing NaN (treated as 0xFFFFFFFF) from tripping the battery safety but lower-bounding them to -100C
+            # This occurs when the measurement is closes to 3V, suggesting that the thermistor isn't connected
+            if thermistor_resistance < 0:
+                output_temperature[i] = -100.0
+
+        return output_temperature
+
+    def read_temp(self, dcc_5: int, dcc_4: int, dcc_3: int, dcc_2: int, dcc_1: int) -> list[float]:
+        """
+        Read temperature from LTC6810
+        :param dcc_5: DCC5 pin
+        :param dcc_4: DCC4 pin
+        :param dcc_3: DCC3 pin
+        :param dcc_2: DCC2 pin
+        :param dcc_1: DCC1 pin
+        :return: list of temperature values
+        """
+        temp_array = [0.0] * BATTERY_NUM_TEMPERATURES
+        data_to_send = [0] * 16
+        for cycle in range(3):
+            if cycle == 0:
+                self.initialize_board(0, 0, 0, dcc_5, dcc_4, dcc_3, dcc_2, dcc_1)  # channel 1
+            elif cycle == 1:
+                self.initialize_board(0, 0, 1, dcc_5, dcc_4, dcc_3, dcc_2, dcc_1)  # channel 2
+            elif cycle == 2:
+                self.initialize_board(0, 1, 0, dcc_5, dcc_4, dcc_3, dcc_2, dcc_1)  # channel 3
+
+            message_in_binary = 0b10100010010  # conversion GPIO1, command AXOW
+            LTC6810.generate_command(message_in_binary, data_to_send)
+            self.spi.transfer(data_to_send[:4])  # send 4 bytes of data
+
+            # receive data
+            message_in_binary = 0b11000000000  # read auxiliary group 1, command RDAUXA
+            LTC6810.generate_command(message_in_binary, data_to_send)
+            data_to_receive = self.spi.transfer(data_to_send)  # receive temp data from LTC6810 via SPI
+
+            # write value into array
+            temp_array[cycle] = 256 * data_to_receive[3] + data_to_receive[2]
+
+        return temp_array
+
+    def read_voltage(self):
+        """
+        Read voltage from LTC6810
+        :return: array of voltage readings
+        """
+        volt_array = [0.0] * BATTERY_NUM_VOLTAGES
+        data_to_send = [0] * 4  # data organized by LTC6810.generate_command()
+        data_to_receive = [0] * 8  # voltage data from LTC6810 via SPI
+
+        # read first half of data
+        vmessage_in_binary = 0b01101110000  # adcv discharge enable,7Hz
+        LTC6810.generate_command(vmessage_in_binary, data_to_send)  # generate the "check voltage command"
+        data_to_receive = self.spi.transfer(data_to_send[:4])
+
+        vmessage_in_binary = 0b100  # read cell voltage reg group 1
+        LTC6810.generate_command(vmessage_in_binary, data_to_send)
+        data_to_receive = self.spi.transfer(data_to_send[:4])
+
+        volt_array[0] = LTC6810.data_to_voltage(data_to_receive[0], data_to_receive[1]) / 10000.0
+        volt_array[1] = LTC6810.data_to_voltage(data_to_receive[2], data_to_receive[3]) / 10000.0
+        volt_array[2] = LTC6810.data_to_voltage(data_to_receive[4], data_to_receive[5]) / 10000.0
+
+        vmessage_in_binary = 0b110  # read cell voltage reg group 2
+        LTC6810.generate_command(vmessage_in_binary, data_to_send)
+        data_to_receive = self.spi.transfer(data_to_send[:4])
+        self.gpio_b.write(True)  # TODO: check if this is still needed
+
+        volt_array[3] = LTC6810.data_to_voltage(data_to_receive[0], data_to_receive[1]) / 10000.0
+        volt_array[4] = LTC6810.data_to_voltage(data_to_receive[2], data_to_receive[3]) / 10000.0
+
+        return volt_array
