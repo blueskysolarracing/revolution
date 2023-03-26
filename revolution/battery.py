0e27cace552c177a3137f0cae1381cefa22cb0d8
Emma Fu <emmafu301@hotmail.com>
2023-03-26 09:41:25 -0400

    Updating to use deque for storing past temp and voltage

--- revolution/battery.py
+++ revolution/battery.py
@@ -1,5 +1,5 @@
 from multiprocessing import get_logger
-from queue import Queue
+from collections import deque
 from enum import Enum, auto
 from dataclasses import dataclass, field
 from functools import partial
@@ -19,12 +19,11 @@ BMS_MODULE_NUM_STATE_OF_CHARGES = BMS_MODULE_NUM_CELLS
 BMS_MODULE_NUM_TEMPERATURE_SENSOR = 3
 BMS_MODULE_NUM_TEMPERATURES = BMS_MODULE_NUM_TEMPERATURE_SENSOR
 
+STATIC_FLOAT_QUEUE_NUM_VALUES = 5
 
-def get_queue_average(queue: Queue[float]) -> float:
-    total = 0.0
-    for element in queue.queue:
-        total += element
-    return 0.0 if queue.qsize() == 0 else total / queue.qsize()
+
+def get_queue_avg(queue: deque[float]) -> float:
+    return sum(list(queue)) / len(list(queue)) if len(list(queue)) > 0 else 0.0
 
 
 @dataclass
@@ -36,8 +35,8 @@ class BMSModule:
     ltc6810: LTC6810 = field(init=False)
     voltage_arr: list[float] = field(init=False)
     temperature_arr: list[float] = field(init=False)
-    past_voltage_arr: list[Queue[float]] = field(default_factory=list)
-    past_temp_arr: list[Queue[float]] = field(default_factory=list)
+    past_voltage_arr: list[deque[float]] = field(default_factory=list)
+    past_temp_arr: list[deque[float]] = field(default_factory=list)
     state_of_charge_arr: list[float] = field(default_factory=lambda: [0.0] * BMS_MODULE_NUM_STATE_OF_CHARGES)
     current: float = 0.0
 
@@ -52,11 +51,11 @@ class BMSModule:
         self.temperature_arr = self.ltc6810.read_temp(0, 0, 0, 0, 0)
 
         for i in range(BMS_MODULE_NUM_VOLTAGES):
-            self.past_voltage_arr.append(Queue())
-            self.past_voltage_arr[i].put(self.voltage_arr[i])
+            self.past_voltage_arr.append(deque(maxlen=STATIC_FLOAT_QUEUE_NUM_VALUES))
+            self.past_voltage_arr[i].append((self.voltage_arr[i]))
 
-            self.past_temp_arr.append(Queue())
-            self.past_temp_arr[i].put(self.temperature_arr[i])
+            self.past_temp_arr.append(deque(maxlen=STATIC_FLOAT_QUEUE_NUM_VALUES))
+            self.past_temp_arr[i].append((self.temperature_arr[i]))
 
         # TODO: initialize battery algo using EKF
 
@@ -66,7 +65,7 @@ class BMSModule:
             if get_mode == BMSModule.GetMode.GET_MOST_RECENT:
                 temperatures[i] = self.temperature_arr[i]
             elif get_mode == BMSModule.GetMode.GET_PAST_AVERAGE:
-                temperatures[i] = get_queue_average(self.past_temp_arr[i])
+                temperatures[i] = get_queue_avg(self.past_temp_arr[i])
             elif get_mode == BMSModule.GetMode.GET_FILTERED_RESULT:
                 pass
 
@@ -76,7 +75,7 @@ class BMSModule:
             if get_mode == BMSModule.GetMode.GET_MOST_RECENT:
                 voltages[i] = self.voltage_arr[i]
             elif get_mode == BMSModule.GetMode.GET_PAST_AVERAGE:
-                voltages[i] = get_queue_average(self.past_voltage_arr[i])
+                voltages[i] = get_queue_avg(self.past_voltage_arr[i])
             elif get_mode == BMSModule.GetMode.GET_FILTERED_RESULT:
                 pass
 
@@ -90,13 +89,13 @@ class BMSModule:
         local_temp_arr = self.ltc6810.voltage_to_temp(local_volt_arr)
         for i in range(BMS_MODULE_NUM_TEMPERATURES):
             self.temperature_arr[i] = local_temp_arr[i]
-            self.past_temp_arr[i].put(local_temp_arr[i])
+            self.past_temp_arr[i].append((local_temp_arr[i]))
 
     def measure_voltage(self) -> None:
         local_voltage_arr = self.ltc6810.read_voltage()
         for i in range(BMS_MODULE_NUM_VOLTAGES):
             self.voltage_arr[i] = local_voltage_arr[i]
-            self.past_voltage_arr[i].put(local_voltage_arr[i])
+            self.past_voltage_arr[i].append((local_voltage_arr[i]))
 
     def compute_soc(self) -> None:
         local_soc_arr = [0.0] * BMS_MODULE_NUM_STATE_OF_CHARGES
