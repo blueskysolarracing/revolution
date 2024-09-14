34767afac1b0af20f625671a190a57b599594a48
Sparsh Kochhar <sparsh.kochhar@gmail.com>
2024-09-14 15:07:15 -0400

    Added unit test for motor cruise control

--- revolution/tests/test_driver.py
+++ revolution/tests/test_driver.py
@@ -42,6 +42,41 @@ class DriverTestCase(TestCase):
 
         driver_thread.join()
 
+    def test_motor_cruise_control_status(self) -> None:
+        environment = Environment(
+            AcquirableDoor(configurations.CONTEXTS),
+            configurations.PERIPHERIES,
+            configurations.SETTINGS,
+        )
+        driver_thread = Thread(target=Driver.main, args=(environment,))
+
+        driver_thread.start()
+        sleep(self.TIMEOUT)
+
+        with environment.contexts() as contexts:
+            self.assertFalse(contexts.motor_cruise_control_status_input)
+        (
+            configurations  # type: ignore[method-assign]
+            .STEERING_WHEEL_MCP23S17
+            .read_register
+        ) = lambda *_: [0]
+
+        sleep(self.TIMEOUT)
+
+        (
+            configurations  # type: ignore[method-assign]
+            .STEERING_WHEEL_MCP23S17
+            .read_register
+        ) = lambda *_: [0xFF]
+
+        sleep(self.TIMEOUT)
+
+        with environment.contexts() as contexts:
+            self.assertTrue(contexts.motor_cruise_control_status_input)
+
+        environment.send_all(Message(Header.STOP))
+        driver_thread.join()
+
 
 if __name__ == '__main__':
     main()  # pragma: no cover
