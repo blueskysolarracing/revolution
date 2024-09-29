00dc671e1a0e16f9649ad65449f276d4aea8e2fc
Sparsh Kochhar <sparsh.kochhar@gmail.com>
2024-09-29 19:58:40 -0400

    added check for OFF state when array relay not held and cruise control toggled off

--- revolution/tests/test_driver.py
+++ revolution/tests/test_driver.py
@@ -30,6 +30,7 @@ class DriverTestCase(TestCase):
         self.driver_thread.join()
 
     def test_array_relay_status(self) -> None:
+        # check that array relay is initially OFF
         with self.environment.contexts() as contexts:
             self.assertFalse(contexts.power_array_relay_status_input)
 
@@ -41,12 +42,26 @@ class DriverTestCase(TestCase):
 
         sleep(self.TIMEOUT)
 
+        # check ON after holding button
         with self.environment.contexts() as contexts:
             self.assertTrue(contexts.power_array_relay_status_input)
+        (
+            configurations  # type: ignore[method-assign]
+            .STEERING_WHEEL_MCP23S17
+            .read_register
+        ) = lambda *_: [0xFF]
+
+        sleep(self.TIMEOUT)
+
+        # check OFF after releasing button
+        with self.environment.contexts() as contexts:
+            self.assertFalse(contexts.power_array_relay_status_input)
 
     def test_motor_cruise_control_status(self) -> None:
+        # check that cruise control is initially OFF
         with self.environment.contexts() as contexts:
             self.assertFalse(contexts.motor_cruise_control_status_input)
+
         (
             configurations  # type: ignore[method-assign]
             .STEERING_WHEEL_MCP23S17
@@ -54,6 +69,10 @@ class DriverTestCase(TestCase):
         ) = lambda *_: [0]
 
         sleep(self.TIMEOUT)
+
+        # check ON after first toggle
+        with self.environment.contexts() as contexts:
+            self.assertTrue(contexts.motor_cruise_control_status_input)
 
         (
             configurations  # type: ignore[method-assign]
@@ -63,8 +82,9 @@ class DriverTestCase(TestCase):
 
         sleep(self.TIMEOUT)
 
+        # check OFF after second toggle
         with self.environment.contexts() as contexts:
-            self.assertTrue(contexts.motor_cruise_control_status_input)
+            self.assertFalse(contexts.motor_cruise_control_status_input)
 
 
 if __name__ == '__main__':
