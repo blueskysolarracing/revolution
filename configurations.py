13016913c8cae5f61a257a8766cbc89420fd0a2c
chenghymichael <chenghymichael.acc@gmail.com>
2025-07-03 13:03:34 -0500

    Adjust motor accelerator

--- configurations.py
+++ configurations.py
@@ -124,7 +124,7 @@ CONTEXTS: Contexts = Contexts(
 
 CAN_BUS_CHANNEL: str = 'can0'
 CAN_BUS_TXQUEUELEN: int = 1000
-CAN_BUS_BITRATE: int = 500000
+CAN_BUS_BITRATE: int = 125000
 
 system(f'ip link set {CAN_BUS_CHANNEL} down')
 system(
@@ -506,9 +506,9 @@ SETTINGS: Settings = Settings(
     motor_control_timeout=0.1,
     motor_variable_field_magnet_timeout=0.1,
 
-    motor_acceleration_input_max_increase=0.1,
-    motor_acceleration_input_max_decrease=0.3,
-    motor_bus_current_limit=0.5,
+    motor_acceleration_input_max_increase=1,
+    motor_acceleration_input_max_decrease=1,
+    motor_bus_current_limit=0.7,
     motor_regeneration_strength=0.3,
     motor_variable_field_magnet_step_size=40,
     motor_variable_field_magnet_step_upper_limit=320,
@@ -536,7 +536,7 @@ SETTINGS: Settings = Settings(
 
     # Telemetry
 
-    telemetry_timeout=1,
+    telemetry_timeout=0.2,
     telemetry_begin_token=b'',
     telemetry_separator_token=b'_',
     telemetry_end_token=b'\r\n',
--- revolution/tests/configurations.py
+++ revolution/tests/configurations.py
@@ -309,7 +309,7 @@ SETTINGS: Settings = Settings(
 
     # Display
 
-    display_frame_rate=1,
+    display_frame_rate=3,
     display_font_pathname='fonts/minecraft.ttf',
 
     # Driver
@@ -328,9 +328,9 @@ SETTINGS: Settings = Settings(
     motor_control_timeout=0.1,
     motor_variable_field_magnet_timeout=0.1,
 
-    motor_acceleration_input_max_increase=0.1,
-    motor_acceleration_input_max_decrease=0.3,
-    motor_bus_current_limit=0.5,
+    motor_acceleration_input_max_increase=1,
+    motor_acceleration_input_max_decrease=1,
+    motor_bus_current_limit=0.7,
     motor_regeneration_strength=0.3,
     motor_variable_field_magnet_step_size=40,
     motor_variable_field_magnet_step_upper_limit=320,
@@ -358,7 +358,7 @@ SETTINGS: Settings = Settings(
 
     # Telemetry
 
-    telemetry_timeout=1,
+    telemetry_timeout=0.2,
     telemetry_begin_token=b'',
     telemetry_separator_token=b'_',
     telemetry_end_token=b'\r\n',
