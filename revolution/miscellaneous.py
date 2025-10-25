b074fbfc62c985dc661a1769d7ccc75dc49223a1
chenghymichael <chenghymichael.acc@gmail.com>
2025-10-24 21:59:30 -0400

    Fixed imu log (5)

--- revolution/miscellaneous.py
+++ revolution/miscellaneous.py
@@ -407,13 +407,13 @@ class Miscellaneous(Application):
 
             print(f'{datetime.now().time()}, '
                   f'{left_accel.x}, {left_accel.y}, {left_accel.z}, '
-                  f'{right_accel.x}, {right_accel.y}, {right_accel.z}'
+                  f'{right_accel.x}, {right_accel.y}, {right_accel.z}, '
                   f'{imu.get('x', 0.0)}, {imu.get('y', 0.0)}, '
                   f'{imu.get('z', 0.0)}'
             )
             print(f'{datetime.now().time()}, '
                   f'{left_accel.x}, {left_accel.y}, {left_accel.z}, '
-                  f'{right_accel.x}, {right_accel.y}, {right_accel.z}'
+                  f'{right_accel.x}, {right_accel.y}, {right_accel.z}, '
                   f'{imu.get('x', -100)}, {imu.get('y', -100)}, '
                   f'{imu.get('z', -100)}',
                   file=log_file
