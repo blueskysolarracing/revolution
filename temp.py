c0d54d98b02e1d4459aff89fbbd81153b33f4d38
chenghymichael <chenghymichael.acc@gmail.com>
2025-10-25 05:48:41 -0400

    Temp.py accelerometer

--- temp.py
+++ temp.py
@@ -3,10 +3,7 @@ from time import sleep
 while True:
     with environment.contexts() as ctx:
         print(
-            f"HV voltage={ctx.power_battery_HV_bus_voltage:.5f} "
-            f"HV current={ctx.power_battery_HV_current:.5f} | "
-            f"LV voltage={ctx.power_battery_LV_bus_voltage:.5f} "
-            f"LV current={ctx.power_battery_LV_current:.5f} | "
-            f"supp voltage={ctx.power_battery_supp_voltage:.5f}"
+            f"L:{ctx.miscellaneous_left_wheel_accelerations} | "
+            f"R:{ctx.miscellaneous_right_wheel_accelerations}"
         )
     sleep(0.5)
