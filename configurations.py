65331dcfa634947c2c42b548907162e8b54f3a7d
chenghymichael <chenghymichael.acc@gmail.com>
2025-11-04 21:28:53 -0500

    Fixed log path

--- configurations.py
+++ configurations.py
@@ -510,7 +510,7 @@ SETTINGS: Settings = Settings(
     miscellaneous_orientation_timeout=0.1,
     miscellaneous_position_timeout=1,
     miscellaneous_front_wheels_timeout=0.02,
-    miscellaneous_acceleration_log_filepath='/usr/share/revolution_logs',
+    miscellaneous_acceleration_log_filepath='/usr/share/revolution_logs/',
 
     # Motor
 
